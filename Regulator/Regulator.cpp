4dee7c66f7ad5a803038853bc3ccb2c4a497ce87
zenTK <56695055+zenTK@users.noreply.github.com>
2019-11-27 12:21:33 +0100

    Add files via upload

--- Regulator/Regulator.cpp
+++ Regulator/Regulator.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+ï»¿#include <iostream>
 #include <fstream>
 #include <string>
 #include <sstream>
@@ -7,9 +7,9 @@
 
 using namespace std;
 
-int zelenaTemp; // želena Temparatura
-int zelenaVlaz; // želena vlaznost
-int zelenaOsv; // želena osvetljenost
+int zelenaTemp; // ï¿½elena Temparatura
+int zelenaVlaz; // ï¿½elena vlaznost
+int zelenaOsv; // ï¿½elena osvetljenost
 int trenutnaTemp;
 int trenutnaVlaz;
 int trenutnaOsv;
@@ -35,10 +35,11 @@ void vnesiZeleneLastnosti() {
 	cout << "\nOsvetljenost :";
 	cin >> zelenaOsv;
 
-	/*while (!(zelenaTemp >= 10) && !(zelenaTemp <= 30) && !(zelenaTemp > 30) && !(zelenaTemp <= 90) && !(zelenaTemp >= 283) && !(zelenaTemp <= 303)) {
-		cout << "Izberi temparaturo med 10 C in 30C, 31F in 90F ali 283 K in 303K";
+	while ((zelenaTemp < 10) || ((zelenaTemp > 90) && (zelenaTemp < 283)) || (zelenaTemp > 303)) {
+		cout << "Izberi temparaturo med 10 C in 30C, 31F in 90F ali 283 K in 303K\n";
+		cout << "Temperatura: ";
 		cin >> zelenaTemp;
-	}*/
+	}
 
 	if (zelenaTemp <= 30 && zelenaTemp >= 10) {
 		zelenaCelzij = zelenaTemp;
@@ -53,6 +54,7 @@ void vnesiZeleneLastnosti() {
 	}
 }
 
+
 void vnesiTrenutneLastnosti() {
 	cout << "Vnesi trenutne lastnosti\n";
 	cout << "\tTemperatura[C, K in F]: "; // simbol za stopinje ne dela v cmd-ju
@@ -65,18 +67,24 @@ void vnesiTrenutneLastnosti() {
 	cin >> trenutnaOsv;
 	cout << endl;
 
+	while ((trenutnaTemp < 10) || ((trenutnaTemp > 90) && (trenutnaTemp < 283)) || (trenutnaTemp > 303)) {
+		cout << "Izberi temparaturo med 10 C in 30C, 31F in 90F ali 283 K in 303K\n";
+		cout << "Temperatura: ";
+		cin >> zelenaTemp;
+	}
+
 	if (trenutnaTemp <= 30 && trenutnaTemp >= 10) {
-		trenutnaTemp = trenutnaCelzij;
+		trenutnaCelzij = trenutnaTemp;
 	}
 	else if (trenutnaTemp > 30 && trenutnaTemp <= 90) {
-		trenutnaTemp = trenutnaFahrenheit;
-		trenutnaFahrenheit = trenutnaFahrenheit * 1.8 + 32;
+		trenutnaFahrenheit = trenutnaTemp;
 	}
 	else if (trenutnaTemp >= 283 && trenutnaTemp <= 303) {
-		trenutnaTemp = trenutnaKelvin;
-		trenutnaKelvin += 273.15;
+		trenutnaKelvin = trenutnaCelzij;
 	}
 }
+
+
 
 void glavniMeni() {
 	cout << "GLAVNI MENI" << endl;
@@ -84,6 +92,40 @@ void glavniMeni() {
 	cout << "1.) Rocni nacin\n" << "2.) Avtomatski nacin\n" << "3.) Avtomatski nacin 2\n" << "4.) Pomoc\n" << "0.) Izhod" << endl;
 }
 
+void datoteka() {
+	ofstream mojaDatOut;
+	mojaDatOut.open("Zeljene_ambientalne_lastnosti.txt");
+	if (mojaDatOut.is_open()) {
+		mojaDatOut << "TEMPERATURA[C, F ali K]: " << zelenaTemp;
+		if (zelenaTemp <= 30 && zelenaTemp >= 10) {
+			mojaDatOut << "[C]";
+		}
+		else if (zelenaTemp > 30 && zelenaTemp <= 90) {
+			mojaDatOut << "[F]";
+		}
+		else if (zelenaTemp >= 283 && zelenaTemp <= 303) {
+			mojaDatOut << "[K]";
+		}
+
+		mojaDatOut << "\nVLAZNOST[%]: " << zelenaVlaz << " %\nOSVETLJENOST[lx]: " << zelenaOsv << " lx\n";
+		mojaDatOut << "\nINTERVAL TEMPERATURE: ï¿½C\nSTOPNJA VLAZNOSTI: %\nINTERVAL OSVETLJENOSTI: lx";
+		mojaDatOut.close();
+	}
+	else {
+		cout << "Napaka pri odpiranju";
+	}
+
+	ifstream mojaDatIn;
+	mojaDatIn.open("Zeljene_ambientalne_lastnosti.txt");
+	if (mojaDatIn.is_open()) {
+		cout << "Ambientalne lastnosti bodo prilagojene na: \nTemperatura: " << zelenaTemp << "\nVlaznost: " << zelenaVlaz << "\nOsvetljenost: " << zelenaOsv << "\n";
+		mojaDatIn.close();
+	}
+	else
+	{
+		cout << "Napaka pri branju";
+	}
+}
 
 int main(int argc, char* argv[])
 {
@@ -92,11 +134,13 @@ int main(int argc, char* argv[])
 	vnesiZeleneLastnosti();
 	prelom();
 
-	ofstream mojaDatOut;
+	datoteka();
+
+	/*ofstream mojaDatOut;
 	mojaDatOut.open("Zeljene_ambientalne_lastnosti.txt");
 	if (mojaDatOut.is_open()) {
-		mojaDatOut << "TEMPERATURA: " << zelenaTemp << " °C, K, °F\nVLAZNOST: " << zelenaVlaz << " %\nOSVETLJENOST: " << zelenaOsv << " lx\n";
-		mojaDatOut << "\nINTERVAL TEMPERATURE: °C\nSTOPNJA VLAZNOSTI: %\nINTERVAL OSVETLJENOSTI: lx";
+		mojaDatOut << "TEMPERATURA: " << zelenaTemp << " ï¿½C, K, ï¿½F\nVLAZNOST: " << zelenaVlaz << " %\nOSVETLJENOST: " << zelenaOsv << " lx\n";
+		mojaDatOut << "\nINTERVAL TEMPERATURE: ï¿½C\nSTOPNJA VLAZNOSTI: %\nINTERVAL OSVETLJENOSTI: lx";
 		mojaDatOut.close();
 	}
 	else {
@@ -115,7 +159,7 @@ int main(int argc, char* argv[])
 		cout << "Napaka pri branju";
 		return 0;
 	}
-
+	*/
 	prelom();
 
 	if (argc == 1) {
@@ -147,6 +191,7 @@ int main(int argc, char* argv[])
 
 	}
 	else if ((argc == 2) && (strcmp(argv[1], "-t") == 0)) {
+		cout << "\t\nPOZDRAVLJEN/A V TESTNEM NACINU";
 
 
 		return 0;
